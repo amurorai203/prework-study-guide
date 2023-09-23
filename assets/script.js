f4a2b7572b9718ff69d3885f6c33ddb1d599fabd
amurorai203 <laihochi@gmail.com>
2023-09-23 10:34:08 +0100

    Test Javascript loop

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+/*
+var topics = "HTML, CSS, Git, Javascript";
+console.log(topics);
+
+var topic = "HTML"
+
+if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+    console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+    console.log("Let's study Git!");
+} else if (topic === 'Javascript') {
+    console.log("Let's study Javascript!");
+} else {
+    console.log('Please try again!');
+}
+*/
+
+var shapes = ["triangle", "square", "pentagon", "circle"];
+
+for (var x=0; x < shapes.length; x++) {
+    console.log(shapes[x]);
+}
